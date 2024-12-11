7cc5829a114a206f55359536fa3b10d1e4f8b6a8
Zhen <iago.merida@enti.cat>
2024-12-11 12:59:39 +0100

    Hello userName.
    Hemos cambiado el codigo para que salude a un nombre especificado.

--- GitTestSol/GitTest/Source.cpp
+++ GitTestSol/GitTest/Source.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 void main() {
-	std::cout << "Hello World!";
+	std::string userName;
+
+	std::cout << "Your name: ";
+	std::cin >> userName;
+	std::cout << "Hello " << userName;
 };
